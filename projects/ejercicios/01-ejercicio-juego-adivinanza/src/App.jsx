4d1e02750ae2ff1e2dc6af8e62b05492593783b9
MarceloHarp <elkuna10@gmail.com>
2025-11-16 18:01:39 -0300

    Primer commit del juego de adivinanza

--- projects/ejercicios/01-ejercicio-juego-adivinanza/src/App.jsx
+++ projects/ejercicios/01-ejercicio-juego-adivinanza/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 function Header() {
   return (
@@ -23,6 +24,28 @@ function Juego({ maximo }) {
   // - Si no => mostrar mensaje de error
   // - Siempre generar un nuevo número aleatorio con Math.floor(Math.random() * maximo) + 1
 
+  const [numeroJugador, setNumeroJugador] = useState('');
+  const [numeroMaquina] = useState(() => Math.floor(Math.random() * maximo) + 1);
+  const [resultado, setResultado] = useState('');
+  const [mostrarReintento, setMostrarReintento] = useState(false);
+
+  const ResultadoAdivinanza = () => {
+    const intento = parseInt(numeroJugador, 10);
+    if (intento === numeroMaquina) {
+      setResultado('¡Correcto! Adivinaste el número ');
+    } else {
+      setResultado('Como dijo José Luis Félix Chilavert... Tú no has ganado nada!.');
+    }
+    setMostrarReintento(true);
+  };
+  const reiniciarJuego = () => {
+    setNumeroJugador('');
+    setResultado('');
+    setMostrarReintento(false);
+  };
+
+
+
   return (
     <div>
       <form>
@@ -32,14 +55,29 @@ function Juego({ maximo }) {
           min="1"
           max={maximo}
           placeholder="Ingresa un número del 1 al 10"
+          value={numeroJugador}
+          onChange={(e) => setNumeroJugador(Number(e.target.value))}
         />
-        <button type="button">Adivinar</button>
+        <button type="button" onClick={ResultadoAdivinanza} >Probar mi suerte</button>
       </form>
+       {resultado && (
+        <div className={`resultado ${resultado.includes('Correcto') ? 'acierto' : 'error'}`}>
+        </div>
+      )}
+       {resultado && !resultado.includes('Correcto') && (
+        <img
+          src="/chila.jpg"
+          alt="Fallaste"
+          style={{ width: '200px', marginTop: '10px' }}
+        />
+      )}
 
       {/* TODO: Mostrar el resultado con una clase dinámica si adivinó */}
-      <div className="resultado">
-        {/* Mostrar el mensaje del resultado aquí */}
+      <div className="resultado" >
+        <strong>{resultado}</strong>
       </div>
+      {mostrarReintento && (
+        <button type="button" onClick={reiniciarJuego}>Volver a intentarlo</button>)}
     </div>
   );
 }
@@ -51,6 +89,7 @@ function App() {
       <Juego maximo={10} />
       <footer>¡Intenta adivinar el número entre 1 y 10!</footer>
     </div>
+
   );
 }
 
